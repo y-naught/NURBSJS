864f1e4d181a02ceeb86ef62f4696d14a9813357
Greg King <gregking917@gmail.com>
2022-08-01 22:45:19 -0400

    Fixed browserify UUID issue

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 tsc
-browserify ./build/main.js -p esmify > ./build/bundle.js
+browserify ./src/main.js -p esmify > ./build/bundle.js
 browserify ./build/index.js -p esmify > ./build/bundle2.js
---
+++ src/main.js
@@ -0,0 +1,1 @@
+import {v4 as uuidv4} from 'uuid';
